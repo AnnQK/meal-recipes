daacf13c42ea32be496f018e8c07ddbe59d3b845
Anna Kovalik <annhuddy13@gmail.com>
2022-12-11 20:14:19 +0300

    Feat: implement redux toolkit

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://www.themealdb.com/api/json/v1/";
+const DEV_API_KEY = "1";
+
+export const getCategories = async () => {
+    const res = await axios.get(`${BASE_URL}${DEV_API_KEY}/categories.php`)
+    return res
+}
+
+export const getIngredients = async () => {
+    const res = await axios.get(`${BASE_URL}${DEV_API_KEY}/list.php?i=list`)
+    return res
+}
+
+export const getRandomRecipe = async () => {
+    const res = await axios.get(`${BASE_URL}${DEV_API_KEY}/random.php`)
+    return res
+}
+
+// TODO: add requests for images
---
+++ src/store/categoriesSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getCategories } from "../api/api";
+
+export const fetchCategories = createAsyncThunk(
+    'categories/fetchCategories',
+
+    async function (_, { rejectWithValue }) {
+        try {
+            const { status, data } = await getCategories();
+            if (status < 200 || status > 300) {
+                throw new Error("Can't load categories")
+            }
+            return data.categories
+
+        } catch (error) {
+            rejectWithValue(error.message)
+        }
+
+    }
+)
+
+
+
+const initialState = {
+    categories: [],
+    status: null,
+    error: null,
+}
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState,
+    extraReducers: {
+        [fetchCategories.pending]: (state) => {
+            state.status = "loading";
+            state.error = null;
+        },
+        [fetchCategories.fulfilled]: (state, action) => {
+            state.categories = action.payload;
+            state.status = "success"
+        },
+        [fetchCategories.rejected]: (state, action) => {
+            state.status = "error";
+            state.error = action.payload;
+        },
+    }
+})
+
+export default categoriesSlice.reducer
